049ec53cded625789d3de4b2b018c11612b3323d
3234ROU <cyril.rousteau@opt.nc>
2024-02-15 10:42:44 +1100

    ajout des routes pour ajouter et afficher l'id

--- src/Controller/MarquePageController.php
+++ src/Controller/MarquePageController.php
@@ -8,17 +8,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\MarquePage;
 use Doctrine\ORM\EntityManagerInterface;
 
+#[Route("/marque-page", requirements: ["_locale" => "en|es|fr"], name: "marque-page_")]
+
 class MarquePageController extends AbstractController
 {
+    #[Route("/", name: "index")]
+    public function index(EntityManagerInterface $entityManager): Response
+    { $marquePages = $entityManager->getRepository(MarquePage::class)->findAll();
 
-#[Route('/liste/marque-page', name: 'app_marque_page')]
-public function liste(EntityManagerInterface $entityManager): Response
-{
-    $marquePages = $entityManager->getRepository(MarquePage::class)->findAll();
+        return $this->render('liste/marque_page.html.twig', [
+            'marquePages' => $marquePages,
+        ]);
+     }
 
-    return $this->render('liste/marque_page.html.twig', [
-        'marquePages' => $marquePages,
-    ]);
+
+     #[Route("/{id<\d+>}", name: "marque-page")]
+     public function afficherDetailMarquePage(int $id, EntityManagerInterface $entityManager): Response
+     {
+         $marquePage = $entityManager->getRepository(MarquePage::class)->find($id);
+         if (!$marquePage) {
+             throw $this->createNotFoundException(
+                 "Aucun marque-page avec l'id ".$id
+             );
+         }
+         return $this->render('marque-page/detail.html.twig', [
+             'marquePage' => $marquePage,
+         ]);
+     }
+
+
+    #[Route("/ajouter", name: "marque-page_ajouter")]
+    public function ajouterMarquePage(EntityManagerInterface $entityManager): Response
+    {
+        
+        $marquePage = new MarquePage();
+        $marquePage->setCommentaire("J'aime Symfony très très beaucoup");
+        $marquePage->setDateCreation(new \DateTime());
+        $marquePage->setUrl("Mon premier url");
+
+        $entityManager->persist($marquePage);
+        $entityManager->flush();
+
+    return new Response("Marque-page sauvegardé avec l'id ". $marquePage->getId());
+    }
 }
-
-}
+    
