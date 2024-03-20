5c23b4c6622c4968e6a8712b352fc8ec7488342d
3234ROU <cyril.rousteau@opt.nc>
2024-03-20 15:29:53 +1100

    AdvengersV8_Part5/5: Ajout des mots clefs dans le formulaire

--- src/Form/Type/AjoutMarquePagesType.php
+++ src/Form/Type/AjoutMarquePagesType.php
@@ -2,6 +2,7 @@
 namespace App\Form\Type;
 
 use App\Entity\MarquePage;
+use App\Entity\MotsCles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -9,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class AjoutMarquePagesType extends AbstractType
 {
@@ -22,6 +24,12 @@ class AjoutMarquePagesType extends AbstractType
             ->add('date_creation', DateTimeType::class, [
                 'widget' => 'single_text',
                 'html5' => true, 
+            ])
+             ->add('motsCles', EntityType::class, [
+                'class' => MotsCles::class,
+                'choice_label' => 'motCle',
+                'multiple' => true,
+                'expanded' => false
             ])
             ->add('save', SubmitType::class, ['label' => 'Enregistrer']);
     }
